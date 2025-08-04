21e9ffa18a8bfc4d29f702ce0a503dc8afecd664
CurtisAda <164827054+CurtisAda@users.noreply.github.com>
2025-08-04 16:03:44 -0500

    tailwind

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button class="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-300">
+    Click Me
+  </button>
       </header>
     </div>
   );
